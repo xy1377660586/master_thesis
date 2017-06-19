e40d873d62800f5a3a9b55af8743db64536622f3
Yan <bupt_xy@126.com>
2017-06-19 10:26:58 -0400

    Update convert to db format.sh

--- convert to db format.sh
+++ convert to db format.sh
@@ -1,3 +1,7 @@
+cd caffe
+sudo gedit finetunning_thesis/data/create_lmdb.sh
+ //then edit the following to your file 
+
 DATA=myfinetunning/data
 echo 'create train lmdb'
 rm -rf $DATA/img_train_lmdb
@@ -8,3 +12,6 @@ rm -rf $DATA/img_test_lmdb
 build/tools/convert_imageset --shuffle / $DATA/test.txt  $DATA/img_test_lmdb
 
 echo 'Done..'
+
+//return to command line execute 
+sudo sh finetunning_thesis/data/create_lmdb.sh
